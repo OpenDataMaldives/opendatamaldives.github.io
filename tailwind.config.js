c5364a85f18bb7a1bf5055e2de8bba6037933ae1
Nattu <nattu@reallynattu.com>
2023-12-08 14:07:55 +0900

    Update font-family and add global.css stylesheet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
+        sans: ["RedditSans-regular", "Inter", ...defaultTheme.fontFamily.sans],
         'opdmv-bold': ['RedditSans-bold', 'sans-serif'],
         'opdmv-italic': ['RedditSans-italic', 'sans-serif'],
         'opdmv-light': ['RedditSans-light', 'sans-serif'],
