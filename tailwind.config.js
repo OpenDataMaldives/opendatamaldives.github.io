bfbad4bda3e3858a00f6cb354f0013ef3826f55f
Nattu <nattu@reallynattu.com>
2023-12-08 13:39:47 +0900

    Update font styles and heading in government page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'opdmv-bold': ['RedditSans-bold', 'sans-serif'],
+        'opdmv-italic': ['RedditSans-italic', 'sans-serif'],
+        'opdmv-light': ['RedditSans-light', 'sans-serif'],
+        'opdmv-regular': ['RedditSans-regular', 'sans-serif'],
+        'opdmv-semi-bold': ['RedditSans-semi-bold', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
